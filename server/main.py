2924ad4f394cfd40c2e214ebcb24d129a0dbdc1f
Amir <168683244+AmirAbasi05@users.noreply.github.com>
2025-11-30 12:51:29 +0330

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from auth.register import sent_otp, verify_otp
+from home import router as home_router  
 
 app = FastAPI(title="App")
-app.include_router(sent_otp.router, prefix="/auth/register")
-app.include_router(verify_otp.router, prefix="/auth/register")
-app.include_router(users.router, prefix="/home")
+app.include_router(sent_otp.router, prefix="/auth")
+app.include_router(verify_otp.router, prefix="/auth") 
+app.include_router(home_router, prefix="/home")
 #app.include_router(set_profile.router, prefix="/profile")
