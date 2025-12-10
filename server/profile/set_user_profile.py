e8d12e515f42c8a4e14448339c259fef41924e10
Navid <>
2025-12-10 11:31:47 -0500

    added the set_user_profile.py

--- server/profile/set_user_profile.py
+++ server/profile/set_user_profile.py
@@ -1,1 +1,57 @@
- 
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from mongoengine import connect, DoesNotExist
+import os
+from allusers import User, Specialties  
+
+MONGO_URI = os.environ.get("MONGO_URI")
+if not MONGO_URI:
+    raise ValueError("MONGO_URI environment variable is not set!")
+
+connect(host=MONGO_URI)
+
+app = FastAPI()
+
+class UserUpdate(BaseModel):
+    uid: str
+    fname: str
+    lname: str
+    age: int
+    gender: str
+    token: str
+
+def verify_token(uid: str, token: str) -> bool:
+    try:
+        user = User.objects.get(uid=uid)
+        return user.token_value == token
+    except DoesNotExist:
+        return False
+
+@app.post("/update")
+async def update_user(data: UserUpdate):
+    if not verify_token(data.uid, data.token):
+        raise HTTPException(status_code=401, detail="Unauthorized: invalid token")
+
+    result = {}
+    for model, name in [(User, "users"), (Specialties, "specialties")]:
+        try:
+            obj = model.objects.get(uid=data.uid)
+            obj.fname = data.fname
+            obj.lname = data.lname
+            obj.age = data.age
+            obj.gender = data.gender
+            obj.save()
+            result[name] = "updated"
+        except DoesNotExist:
+            obj = model(
+                uid=data.uid,
+                fname=data.fname,
+                lname=data.lname,
+                age=data.age,
+                gender=data.gender,
+                number="09123456789" 
+            )
+            obj.save()
+            result[name] = "created"
+
+    return {"status": "success", "details": result}
