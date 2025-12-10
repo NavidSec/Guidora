d1e90fad59f39ce4553ecebd0d43d21771767727
Navid <>
2025-12-10 12:28:03 -0500

    ping pong added

--- server/main.py
+++ server/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
-from auth.register import sent_otp, verify_otp
-from home import router as home_router  
-
-app = FastAPI(title="App")
-app.include_router(sent_otp.router, prefix="/auth")
-app.include_router(verify_otp.router, prefix="/auth") 
-app.include_router(home_router, prefix="/home")
-#app.include_router(set_profile.router, prefix="/profile")
+from test_router import router as test_router
+from auth.sent_otp import router as sent_otp_router
+from auth.verify_otp import router as verify_otp_router
+from auth.set_info import router as set_info_router
+from home.Homepage import router as homepage_router
+from home.users import router as users_router
+from profile.set_spe_profile import router as spe_profile_router
+from profile.set_user_profile import router as user_profile_router
+from reservation.set_spe_avi_slots import router as spe_slots_router
+from reservation.set_user_slot import router as user_slots_router
+app = FastAPI(title="Guidora App")
+app.include_router(test_router, prefix="/test")
+app.include_router(sent_otp_router, prefix="/auth")
+app.include_router(verify_otp_router, prefix="/auth")
+app.include_router(set_info_router, prefix="/auth")
+app.include_router(homepage_router, prefix="/home")
+app.include_router(users_router, prefix="/home")
+app.include_router(spe_profile_router, prefix="/profile")
+app.include_router(user_profile_router, prefix="/profile")
+app.include_router(spe_slots_router, prefix="/reservation")
+app.include_router(user_slots_router, prefix="/reservation")
